3130df332d88084f9d964a2ea0d599d202943990
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-02-28 14:00:55 +0530

    Update azureapp.tf

--- azureapp.tf
+++ azureapp.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_web_app" "webapp" {
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/SmithaVerity/ABTestingApp"
+  repo_url           = "https://github.com/SmithaVerity/IMS"
   branch             = "main"
   use_manual_integration = true
   use_mercurial      = false
