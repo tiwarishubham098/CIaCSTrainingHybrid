6a5c6872e7abb4659c0f470e79665d6ab16f7548
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-02-28 13:54:31 +0530

    Update awsrds.tf

--- awsrds.tf
+++ awsrds.tf
@@ -15,20 +15,47 @@ provider "aws" {
   secret_key = "your-secret-key"
 }
 
+resource "aws_db_security_group" "iac-db-sg" {
+  name = "IaCDBSecGrp"
+  description = "IaC DB Security Group 3306"
+  vpc_id = "vpc-04de07d7d900e861b"
+
+  // To Allow MySQL port
+  ingress {
+    from_port = 3306
+    protocol = "tcp"
+    to_port = 3306
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 resource "aws_db_instance" "rds_instance" {
-allocated_storage = 20
-identifier = "rds-terraform"
-storage_type = "gp2"
-engine = "mysql"
-engine_version = "5.7.41"
-instance_class = "db.t2.micro"
-name = "iacdb"
-username = "admin"
-password = "admin123"
-publicly_accessible    = true
-skip_final_snapshot    = true
-
+  allocated_storage = 20
+  identifier = "rds-terraform"
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "5.7.41"
+  instance_class = "db.t2.micro"
+  name = "shop_inventory"
+  username = "admin"
+  password = "admin123"
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  
+  vpc_security_group_ids = [
+    aws_db_security_group.iac-db-sg
+  ] 
 
   tags = {
     Name = "ExampleRDSServerInstance"
