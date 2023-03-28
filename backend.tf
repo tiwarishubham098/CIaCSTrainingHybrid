3dc775c7f9c2542658a4cba03a16bca2da430db9
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-03-28 11:21:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-    source  = "hashicorp/aws"
-    version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
   region     = "ap-south-1"
   access_key = "your-access-key"
